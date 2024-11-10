f1ef66b1d10c250f62f778a26c064e7a98f25966
Shashin <shashinmhrzn@gmail.com>
2024-11-10 05:22:34 +0000

    :rocket: [Update] Refactoring utlity script

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from dataset import SpeechDataModule
-from model import ConformerEncoder, LSTMDecoder
+from Conformer import ConformerEncoder, LSTMDecoder
 from utils import GreedyDecoder
 
 
@@ -52,70 +52,78 @@ class ASRTrainer(pl.LightningModule):
 
     def forward(self, x):
         return self.model(x)
-    
-    
+
     def configure_optimizers(self):
         optimizer = optim.AdamW(
             self.model.parameters(),
-            lr=self.args.learning_rate
+            lr=self.args.learning_rate,
+            betas=(0.9, 0.999),
+            eps=1e-8,
+            weight_decay=0.01,
         )
 
         scheduler = {
-            'scheduler': optim.lr_scheduler.CosineAnnealingWarmRestarts(
+            "scheduler": optim.lr_scheduler.CosineAnnealingWarmRestarts(
                 optimizer,
-                T_0=10,         # Number of epochs for the first restart
-                T_mult=1,       # Factor to increase T_0 after each restart
-                eta_min=6e-5    # Minimum learning rate
+                T_0=10,             # Number of epochs for the first restart
+                T_mult=1,           # Factor to increase T_0 after each restart
+                eta_min=7e-6,       # Minimum learning rate
             ),
-            'monitor': 'val_loss'
+            "monitor": "val_loss",
         }
 
         return [optimizer], [scheduler]
-    
-    
+
     def _common_step(self, batch, batch_idx):
         spectrograms, labels, input_lengths, label_lengths, _, _ = batch
 
         # Directly calls forward method of conformer and pass spectrograms
-        output = self(spectrograms)     
+        output = self(spectrograms)
         output = F.log_softmax(output, dim=-1).transpose(0, 1)
-        
+
         # Compute CTC loss
         loss = self.loss_fn(output, labels, input_lengths, label_lengths)
         return loss, output, labels, label_lengths
-    
 
     def training_step(self, batch, batch_idx):
-        loss,_,_,_ = self._common_step(batch, batch_idx)
+        loss, _, _, _ = self._common_step(batch, batch_idx)
 
-        self.log('train_loss', loss, on_step=True, on_epoch=False, prog_bar=True, logger=True, sync_dist=self.sync_dist)
+        self.log(
+            "train_loss",
+            loss,
+            on_step=True,
+            on_epoch=False,
+            prog_bar=True,
+            logger=True,
+            sync_dist=self.sync_dist,
+        )
         return loss
 
-    
     def validation_step(self, batch, batch_idx):
         loss, y_pred, labels, label_lengths = self._common_step(batch, batch_idx)
         self.losses.append(loss)
 
         # Greedy decoding
-        decoded_preds, decoded_targets = GreedyDecoder(y_pred.transpose(0, 1), labels, label_lengths)
-        
+        decoded_preds, decoded_targets = GreedyDecoder(
+            y_pred.transpose(0, 1), labels, label_lengths
+        )
+
         # Calculate metrics
         # NOTE: Comment CER if u want since validation phase takes a lot of time to compute error for each character
         cer_batch = self.char_error_rate(decoded_preds, decoded_targets)
         wer_batch = self.word_error_rate(decoded_preds, decoded_targets)
-        
+
         # Append batch metrics to lists
         self.val_cer.append(cer_batch)
         self.val_wer.append(wer_batch)
 
         # Log final predictions in CometML
-        if batch_idx%100==0:
+        if batch_idx % 100 == 0:
             log_targets = decoded_targets[-1]
             log_preds = {"Preds": decoded_preds[-1]}
             self.logger.experiment.log_text(text=log_targets, metadata=log_preds)
 
-        return {'val_loss': loss}
-
+        return {"val_loss": loss}
 
     def on_validation_epoch_end(self):
         # Calculate averages of metrics over the entire epoch
@@ -124,9 +132,36 @@ class ASRTrainer(pl.LightningModule):
         avg_wer = torch.stack(self.val_wer).mean()
 
         # Log averaged metrics
-        self.log('val_cer', avg_cer, on_step=False, on_epoch=True, prog_bar=True, logger=True, batch_size=self.args.batch_size, sync_dist=self.sync_dist)
-        self.log('val_wer', avg_wer, on_step=False, on_epoch=True, prog_bar=True, logger=True, batch_size=self.args.batch_size, sync_dist=self.sync_dist)
-        self.log('val_loss', avg_loss, on_step=False, on_epoch=True, prog_bar=True, logger=True, batch_size=self.args.batch_size, sync_dist=self.sync_dist)
+        self.log(
+            "val_cer",
+            avg_cer,
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+            logger=True,
+            batch_size=self.args.batch_size,
+            sync_dist=self.sync_dist,
+        )
+        self.log(
+            "val_wer",
+            avg_wer,
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+            logger=True,
+            batch_size=self.args.batch_size,
+            sync_dist=self.sync_dist,
+        )
+        self.log(
+            "val_loss",
+            avg_loss,
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+            logger=True,
+            batch_size=self.args.batch_size,
+            sync_dist=self.sync_dist,
+        )
 
         # Clear the lists for the next epoch
         self.losses.clear()
@@ -135,33 +170,35 @@ class ASRTrainer(pl.LightningModule):
 
 
 def main(args):
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    device = "cuda" if torch.cuda.is_available() else "cpu"
 
     # Prepare dataset
-    data_module = SpeechDataModule(batch_size=args.batch_size,
-                                   train_json=args.train_json,
-                                   test_json=args.valid_json, 
-                                   num_workers=args.num_workers)
+    data_module = SpeechDataModule(
+        batch_size=args.batch_size,
+        train_json=args.train_json,
+        test_json=args.valid_json,
+        num_workers=args.num_workers,
+    )
     data_module.setup()
 
     # Define model hyperparameters
     # https://arxiv.org/pdf/2005.08100 : Table 1 for conformer parameters
     encoder_params = {
-        'd_input': 80,                        # Input features: n-mels
-        'd_model': 144,                       # Encoder Dims
-        'num_layers': 16,                     # Encoder Layers
-        'conv_kernel_size': 31,
-        'feed_forward_residual_factor': 0.5,
-        'feed_forward_expansion_factor': 4,
-        'num_heads': 4,                       # Relative MultiHead Attetion Heads
-        'dropout': 0.1,
+        "d_input": 80,  # Input features: n-mels
+        "d_model": 144,  # Encoder Dims
+        "num_layers": 16,  # Encoder Layers
+        "conv_kernel_size": 31,
+        "feed_forward_residual_factor": 0.5,
+        "feed_forward_expansion_factor": 4,
+        "num_heads": 4,  # Relative MultiHead Attetion Heads
+        "dropout": 0.1,
     }
 
     decoder_params = {
-        'd_encoder': 144,    # Match with Encoder layer
-        'd_decoder': 320,    # Decoder Dim
-        'num_layers': 1,     # Deocder Layer
-        'num_classes': 29,   # Output Classes
+        "d_encoder": 144,  # Match with Encoder layer
+        "d_decoder": 320,  # Decoder Dim
+        "num_layers": 1,  # Deocder Layer
+        "num_classes": 29,  # Output Classes
     }
 
     # Model Instance
@@ -171,43 +208,46 @@ def main(args):
     speech_trainer = ASRTrainer(model=model, args=args)
 
     # NOTE: Comet Logger
-    comet_logger = CometLogger(api_key=os.getenv('API_KEY'),
-                               project_name=os.getenv('PROJECT_NAME'))
+    comet_logger = CometLogger(
+        api_key=os.getenv("API_KEY"), project_name=os.getenv("PROJECT_NAME")
+    )
 
     # NOTE: Define Trainer callbacks
     checkpoint_callback = ModelCheckpoint(
-        monitor='val_loss',
+        monitor="val_loss",
         dirpath="./saved_checkpoint/",
-        filename='ASR-conformer-{epoch:02d}{val_wer:.2f}',
-        save_top_k=3,        # 3 Checkpoints
-        mode='min'
+        filename="ASR-conformer-{epoch:02d}{val_wer:.2f}",
+        save_top_k=3,  # 3 Checkpoints
+        mode="min",
     )
 
     # Trainer Instance
     trainer_args = {
-        'accelerator': device,
-        'devices': args.gpus,
-        'min_epochs': 1,
-        'max_epochs': args.epochs,
-        'precision': args.precision,
-        'check_val_every_n_epoch': 1, 
-        'gradient_clip_val': 1.0,
-        'callbacks': [LearningRateMonitor(logging_interval='epoch'),
-                      EarlyStopping(monitor="val_loss"), 
-                      checkpoint_callback],
-        'logger': comet_logger
+        "accelerator": device,
+        "devices": args.gpus,
+        "min_epochs": 1,
+        "max_epochs": args.epochs,
+        "precision": args.precision,
+        "check_val_every_n_epoch": 1,
+        "gradient_clip_val": 1.0,
+        "callbacks": [
+            LearningRateMonitor(logging_interval="epoch"),
+            EarlyStopping(monitor="val_loss"),
+            checkpoint_callback,
+        ],
+        "logger": comet_logger,
     }
-    
+
     if args.gpus > 1:
-        trainer_args['strategy'] = args.dist_backend
-        
+        trainer_args["strategy"] = args.dist_backend
+
     trainer = pl.Trainer(**trainer_args)
-    
 
     # Train and Validate
     ckpt_path = args.checkpoint_path
-    trainer.fit(speech_trainer, data_module, ckpt_path=ckpt_path) 
+    trainer.fit(speech_trainer, data_module, ckpt_path=ckpt_path)
     trainer.validate(speech_trainer, data_module)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train ASR Model")
@@ -225,7 +265,7 @@ if __name__ == "__main__":
     # General Train Hyperparameters
     parser.add_argument('--epochs', default=30, type=int, help='number of total epochs to run')
     parser.add_argument('--batch_size', default=32, type=int, help='size of batch')
-    parser.add_argument('-lr', '--learning_rate', default=2e-4, type=float, help='learning rate')
+    parser.add_argument('-lr', '--learning_rate', default=5e-5, type=float, help='learning rate')
     parser.add_argument('--precision', default='16-mixed', type=str, help='precision')
     
     # Checkpoint path
